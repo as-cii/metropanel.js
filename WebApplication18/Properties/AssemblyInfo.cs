033845ac1c1865b17b170c9979dc506e4e36e1eb
Antonio Scandurra <as-cii@outlook.com>
2012-10-14 22:47:54 +0200

    renamed some files

--- WebApplication18/Properties/AssemblyInfo.cs
+++ WebApplication18/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebApplication18")]
+[assembly: AssemblyTitle("MetroPanel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApplication18")]
+[assembly: AssemblyProduct("MetroPanel")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
